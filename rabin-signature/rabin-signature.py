0580e22c688452e3380911b8f2aa633e1c042222
arthur <arthur.dewarumez@gmail.com>
2015-05-26 00:38:42 +0200

    Adding rabi key generation.

---
+++ rabin-signature/rabin-signature.py
@@ -0,0 +1,68 @@
+import os
+import sys
+import os.path
+sys.path.append(os.path.join(os.getcwd(), '..',))
+
+import client as clt
+from tools import *
+import random
+from fractions import gcd
+
+RABIN = "/Rabin-signature/"
+CHALLENGE_URL = RABIN + "challenge" + NAME
+
+PRIMES_SECTION = "Primes"
+P_SECTION = "p"
+Q_SECTION = "q"
+
+N = "n"
+M = "m"
+
+def compute_big_prime(size=2048):
+	"""Return a big prime number"""
+	i = 1
+	while True:
+		prime = random.getrandbits(size)
+		if est_premier(prime):
+			return prime
+		else:
+			print("attempt no " + str(i))
+			i+=1
+
+def save_primes(p, q):
+	primes_f = open(filename, 'w')
+	primes_f.writelines("[" + PRIMES_SECTION  + "]" + "\n")
+	primes_f.writelines(P_SECTION + " = " + str(p) + "\n")
+	primes_f.writelines(Q_SECTION + " = " + str(q) + "\n")
+	primes_f.close()
+
+def read_primes():
+	import configparser as cp
+	p_key = cp.ConfigParser()
+	p_key.read("../primes.ini")
+	p = int(p_key[PRIMES_SECTION][P_SECTION])
+	q = int(p_key[PRIMES_SECTION][Q_SECTION])
+	return p, q
+
+def get_message(n):
+	print("Sending public key to the server...")
+	srv = clt.Server(BASE_URL)
+	try:
+		return srv.query(CHALLENGE_URL, {N: n})[M]
+	except clt.ServerError as err:
+		print_serverError_exit(err)
+
+
+if __name__ == "__main__":
+	p = q = n = b = None
+	if not os.path.exists("../primes.ini"):
+		print("Computing big number...")
+		p = compute_big_prime();
+		q = compute_big_prime();
+		save_primes(p, q)
+	else:
+		print("Reading primes numbers from file...")
+		p, q = read_primes()
+	n = p*q
+	b = random.randint(2, n - 1)
+	m = get_message(n)
